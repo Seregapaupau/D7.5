92ccad2c0e911583cf4854b43a2e97363365619c
Name <email@example.ru>
2022-03-31 22:06:30 +0500

    test
    test

---
+++ NewsPaper/news/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', PostList.as_view()),
+    path('<int:pk>', PostDetail.as_view()),
+    path('search/', PostSearch.as_view(), name='search'),
+    path('add/', PostCreate.as_view(), name='add'),
+    path('edit/<int:pk>', PostUpdate.as_view(), name='edit'),
+    path('delete/<int:pk>', PostDelete.as_view(), name='delete'),
+    path('<int:pk>/add_subscribe/', add_subscribe, name='add_subscribe'),
+    path('<int:pk>/del_subscribe/', del_subscribe, name='del_subscribe'),
+    path('', IndexView.as_view()),
+]
---
+++ NewsPaper/news/views.py
@@ -0,0 +1,106 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import PermissionRequiredMixin  # модуль Д5, чтоб ограничить права доступа
+from django.core.mail import EmailMultiAlternatives
+from django.shortcuts import redirect
+from django.template.loader import render_to_string
+from django.views.generic import ListView, UpdateView, CreateView, DetailView, DeleteView
+
+from .models import Post, Category
+from .filters import PostFilter
+from datetime import datetime
+from .forms import PostForm
+
+from django.http import HttpResponse
+from django.views import View
+from .tasks import hello, send_mail_for_sub_test
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'flatpages/news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-dateCreation')
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['filter'] = PostFilter(
+            self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'flatpages/post.html'
+    queryset = Post.objects.all()
+
+
+class PostSearch(PostList):
+    template_name = 'flatpages/search.html'
+    context_object_name = 'search'
+    filter_class = PostList
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # вписываем наш фильтр в контекст странички
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+class PostCreate(CreateView):
+    template_name = 'flatpages/add.html'
+    form_class = PostForm
+
+
+class PostUpdate(UpdateView):
+    template_name = 'flatpages/edit.html'
+    form_class = PostForm
+
+    # метод get_object мы используем вместо queryset, чтобы получить информацию об объекте, который мы собираемся редактировать
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+
+# дженерик для удаления товара
+class PostDelete(DeleteView):
+    template_name = 'flatpages/delete.html'
+    queryset = Post.objects.all()
+    success_url = '/news/delete/'
+
+
+@login_required
+def add_subscribe(request, **kwargs):
+    pk = request.GET.get('pk', )
+    print('Пользователь', request.user, 'добавлен в подписчики категории:', Category.objects.get(pk=pk))
+    Category.objects.get(pk=pk).subscribers.add(request.user)
+    return redirect('/news/')
+
+
+@login_required
+def del_subscribe(request, **kwargs):
+    pk = request.GET.get('pk', )
+    print('Пользователь', request.user, 'удален из подписчиков категории:', Category.objects.get(pk=pk))
+    Category.objects.get(pk=pk).subscribers.remove(request.user)
+    return redirect('/news/')
+
+
+class AddNews(PermissionRequiredMixin, PostCreate):
+    permission_required = ('news.add',)
+
+
+class ChangeNews(PermissionRequiredMixin, PostUpdate):
+    permission_required = ('news.edit',)
+
+
+class DeleteNews(PermissionRequiredMixin, PostDelete):
+    permission_required = ('news.delete',)
+
+
+class IndexView(View):
+    def get(self, request):
+        # printer.apply_async([10], countdown=10)
+        # hello.delay()
+        send_mail_for_sub_test.delay()
+        return HttpResponse('Hello!')
